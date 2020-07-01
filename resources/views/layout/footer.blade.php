f3dd7b6f74ff0155ac03083a9e2413ca0656e997
Muhammad Rifqy Fakhrul Hadi <hadimrifqyfakhrul@gmail.com>
2020-07-01 17:21:30 +0700

    commit pertama

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,24 @@
+<footer class="main-footer">
+  <div class="float-right d-none d-sm-block">
+    <b>Version</b> 3.1.0-pre
+  </div>
+  <strong>Copyright &copy; 2020 <a href="https://Sanbercode.com">Sanbercode.com</a>.</strong> All rights reserved.
+</footer>
+
+<!-- Control Sidebar -->
+<aside class="control-sidebar control-sidebar-dark">
+  <!-- Control sidebar content goes here -->
+</aside>
+<!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="{{asset('/adminlte/plugins/jquery/jquery.min.js')}}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{asset('/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<!-- AdminLTE App -->
+<script src="{{asset('/adminlte/dist/js/adminlte.min.js')}}"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="{{asset('/adminlte/dist/js/demo.js')}}"></script>
+@stack('scripts')
---
+++ resources/views/master.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>SanberCode | @yield('title')</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href=" {{ asset('/adminlte/plugins/fontawesome-free/css/all.min.css') }} ">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="{{ asset('/adminlte/dist/css/adminlte.min.css') }} ">
+</head>
+
+<body class="hold-transition sidebar-mini">
+  <!-- Site wrapper -->
+  <div class="wrapper">
+    @include('layout.nav')
+    @include('layout.sidebar')
+
+
+
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+      @yield('content')
+    </div>
+    <!-- /.content-wrapper -->
+
+    @include('layout.footer')
+</body>
+
+</html>
